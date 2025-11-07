7818a281b2e617bf743948cd954442eb840b776b
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2025-11-07 10:36:49 +0200

    feat: modify module to accept return types

--- src/base_v2x_feature.py
+++ src/base_v2x_feature.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import gymnasium as gym # used in the future
+import numpy as np
 
 class BaseV2XFeature(ABC):
     def __init__(self, enabled=True):
@@ -7,23 +8,23 @@ class BaseV2XFeature(ABC):
         self.weight = 1.0
 
     @abstractmethod
-    def get_observation_space(self):
+    def get_observation_space(self) -> gym.Space:
         pass
 
     @abstractmethod
-    def get_action_space(self):
+    def get_action_space(self) -> gym.Space:
         pass
 
     @abstractmethod
-    def take_action(self, action):
+    def take_action(self, action) -> None:
         pass
 
     @abstractmethod
-    def get_observation(self):
+    def get_observation(self) -> np.ndarray:
         pass
 
     @abstractmethod
-    def calculate_reward(self):
+    def calculate_reward(self)-> float:
         pass
 
     def feature_step(self):
