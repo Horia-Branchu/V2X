acf7921a80f15a36af314081ff8710e53335ebf1
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2025-11-19 15:01:08 +0200

    fix: make training on CPU only due to constraints

--- src/agents/ppo.py
+++ src/agents/ppo.py
@@ -1,9 +1,12 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Force CPU usage
+
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from base_sumo_env import BaseSumoEnvironment
 
 # create the test enviroment ( will be merged with mihai )
-env = BaseSumoEnvironment("config/simulation.sumocfg") # this will error out
+env = BaseSumoEnvironment("config/simulation.sumocfg", gui=False) # this will error out
 check_env(env)  # Validate your environment
 
 # init the model
@@ -11,7 +14,9 @@ model = PPO("MlpPolicy", env, verbose=1,
             learning_rate=0.0003,
             n_steps=2048,
             batch_size=64,
-            n_epochs=10)
+            n_epochs=10,
+            # Sumo and traci runs only on CPU so it stands to reason that we should not use GPU
+            device='cpu')
 
 # train
 model.learn(total_timesteps=100000)
@@ -21,7 +26,7 @@ model.save("sumo_base_ppo")
 
 # test the trained model
 obs, _ = env.reset()
-for _ in range(1000):
+for _ in range(2):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
