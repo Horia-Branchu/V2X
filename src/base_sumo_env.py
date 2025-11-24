fcf1adddc45dcaee7f237b7e60392ce26b1352cd
Macarescu Denis Gabriel <denis.macarescu@stud.ubbcluj.ro>
2025-11-24 20:27:16 +0200

    added priority feature

--- src/base_sumo_env.py
+++ src/base_sumo_env.py
@@ -11,6 +11,7 @@ import time
 from dummy_feature import DummyFeature
 from dynamic_tls import DynamicTLS
 from bsm_feature import BSMFeature
+from priority_corridor import PriorityCorridorFeature
 
 # use a named logger for the project; features can log at DEBUG for RL and INFO for rule-based
 logger = logging.getLogger("v2x")
@@ -61,7 +62,7 @@ class BaseSumoEnvironment(gym.Env):
         if tls:
             self.features.append(DynamicTLS("DynamicTLS"))
         if priority:
-            self.features.append(DummyFeature("PriorityFeature"))
+            self.features.append(PriorityCorridorFeature("PriorityCorridorFeature"))
         if reroute:
             self.features.append(DummyFeature("RerouteFeature"))
 
