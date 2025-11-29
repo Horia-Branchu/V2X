34cd1ecca716d4fc140b5fe18d365469e5df950b
Teodora Elena Catanas <titocatanas@gmail.com>
2025-11-29 19:50:37 +0200

    feature: dynamic tls continuation

--- src/rl_dynamic_tls.py
+++ src/rl_dynamic_tls.py
@@ -8,14 +8,14 @@ logger = logging.getLogger("v2x.features")
 
 class RLDynamicTLS(BaseSumoEnvironment):
     def __init__(self, feature_name="RL_DynamicTLS", enable=True):
-        super().__init__(enabled)
+        super().__init__(enable)
         self.feature_name = feature_name
         self.observation_size = 5
         self.action_size = 4
         self.phase_time = 0
-        self.extended_sex = 5
+        self.extended_sec = 5
 
-    def get_observation _space(self):
+    def get_observation_space(self):
         return gym.spaces.Box(
             low=0,
             high=200,
@@ -58,3 +58,54 @@ class RLDynamicTLS(BaseSumoEnvironment):
         
         logger.debug(f"[{self.feature_name}] Reward = {reward:.3f}")
         return reward
+
+    def take_action(self, action):
+        tls_list=traci.trafficlight.getIDList()
+
+        if not isinstance(action, (list, tuple, np.ndarray)):
+            action = [action]
+        if len(action) != len(tls_list):
+            logger.warning(
+                f"[{self.feature_name}] Action size {len(action)} does not match "
+                f"TLS count {len(tls_list)}. Broadcasting first action.")
+            action = [action[0]] * len(tls_list)
+
+        for i,tls in enumerate(tls_list):
+            action = action[i]
+            current_pahase=traci.trafficlight.getPhase(tls) 
+
+            if action == 0:
+                pass
+
+            elif action == 1:
+                state = traci.trafficlight.getRedYellowGreenState(tls)
+                if "G" in state:
+                    remaining=traci.trafficlight.getPhaseDuration(tls)
+                    traci.trafficlight.setPhaseDuration(tls, remaining + self.extended_sec)
+
+            elif action == 2:   
+                traci.trafficlight.setPhaseDuration(tls, 0)
+                self.phase_time[tls]=0
+
+            elif action == 3:
+                next_phase = (current_pahase+1) %traci.trafficlight.getPhaseNumber(tls)
+                traci.trafficlight.setPhase(tls, next_pase)
+                self.phase_time[tls]=0
+
+            else:
+                logger.warning(f"[{self.feature_name}] Invalid action: {action}")
+            
+            self.phase_time[tls] += 1
+            next_phase=traci.trafficlight.getPhase(tls)
+            logger.debug(f"[{self.feature_name}] Action taken: {action} on TLS: {tls},"
+                         f" from phase {current_pahase} to {next_phase}, time: {self.phase_time[tls]}")
+
+    def get_feature_name(self):
+        return self.feature_name
+
+    def feature_step(self):
+        pass
+
+    def feature_reset(self):
+        self.phase_time = 0
+        logger.debug(f"[{self.feature_name}] Reset")
